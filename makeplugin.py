84d37c331d4ce43f7ae03154fc2e9bbc825032ad
natruok <138245339+natruok@users.noreply.github.com>
2026-05-21 01:26:52 -0700

    v1 update name

--- makeplugin.py
+++ makeplugin.py
@@ -21,7 +21,7 @@ from glob import glob
 from makezip import createZipFile
 
 if __name__=="__main__":
-    filename="FanFicFare_edited v0.zip"
+    filename="FanFicFare_edited v1.zip"
     exclude=['*.pyc','*~','*.xcf','*[0-9].png','*.po','*.pot','*default.mo','*Thumbs.db']
     
     os.chdir('calibre-plugin')
