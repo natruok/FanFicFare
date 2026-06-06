d06f8ec0645be1e7c011b29f42e65bf4fb124c06
natruok <138245339+natruok@users.noreply.github.com>
2026-06-05 20:13:38 -0700

    Revert "Updated "series_name" metadata to include only name, no index"
    This reverts commit 05d4ea2337e712c3b76a1ef1ad827a72cec261f0.

--- fanficfare/adapters/base_adapter.py
+++ fanficfare/adapters/base_adapter.py
@@ -610,8 +610,7 @@ try to download.</p>
             num = float(num)
             if num.is_integer():
                 num = int(num)
-            #self.story.setMetadata('series','%s [%s]'%(name, num))
-            self.story.setMetadata('series','%s'%(name))
+            self.story.setMetadata('series','%s [%s]'%(name, num))
 
     def setDescription(self,url,svalue):
         #print("\n\nsvalue:\n%s\n"%svalue)
